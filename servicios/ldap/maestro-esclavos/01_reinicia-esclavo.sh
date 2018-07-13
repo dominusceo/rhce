a9d3bb0b500b1da857cd691ec9812c298bcffc90
CARRILLO SANCHEZ RICARDO DAVID <ricardo.carrillo@ine.mx>
2018-07-13 18:11:40 -0500

    Adding LDAP Master-slave configuration

--- servicios/ldap/maestro-esclavos/01_reinicia-esclavo.sh
+++ servicios/ldap/maestro-esclavos/01_reinicia-esclavo.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 HOME_LDAP=/root/maestro-esclavo
-HOME_LDIFSC="$HOME_LDAP/conf-ldifs/"
-HOME_LDIFSD="$HOME_LDAP/data-ldifs/"
 DB_LDAP=/var/lib/ldap
 DB_CONFIG=DB_CONFIG
 SLAPD=$(ps -fea | grep -i slapd|awk '{print $2}')
@@ -18,11 +16,13 @@ function verify_ldap_rules(){
 }
 
 function validating_rsyslog_ldap(){
+
     verify=$(egrep -i -e "ldap\.log")
     if [[ -z "$verify"  && "$verify" = ""  ]] ; then
         echo "local4.* /var/log/ldap.log" | tee -a /etc/rsyslog.conf
         systemctl restart rsyslog && systemctl status rsyslog  &&  systemctl restart slapd
     fi
+
 }
 echo "Reiniciando base de datos..." && sleep 1
 cd $DB_LDAP && rm -fv *.bdb log* __db.00* alock && sleep 1.2
@@ -39,7 +39,7 @@ echo "set_lk_max_objects     1000000"               | tee -a ${DB_LDAP}/$DB_CONF
 echo "set_lg_max             20971520"              | tee -a ${DB_LDAP}/$DB_CONFIG
 echo "set_lg_dir             /var/lib/ldap"         | tee -a ${DB_LDAP}/$DB_CONFIG
 
-# Verifiying iptables rules related ldap service, if exist any rule won't
+# Verifiying iptables rules related ldap service, if exist any rule won't 
 # apply any rule, otherwise will be applied ldap rules.
 verify_ldap_rules
 chown -Rv ldap.ldap $DB_LDAP
@@ -51,34 +51,32 @@ yum -y reinstall openldap*
 systemctl restart slapd && sleep 1.2 && systemctl enable slapd
 # Loading into configuration, ldap modules
 cd /etc/openldap/schema/
-for i in $(echo cosine inetorgperson nis misc openldap ppolicy) ; do
-        ldapadd -Y EXTERNAL -H ldapi:/// -D "cn=config" -f ${i}.ldif;
+for i in $(echo cosine inetorgperson nis misc openldap ppolicy) ; do 
+        ldapadd -Y EXTERNAL -H ldapi:/// -D "cn=config" -f ${i}.ldif; 
 done
+
 # Verifiying ldap loggin information, so we enable these feature
 # adding configuration into rsyslog configuration file.
 validating_rsyslog_ldap
+
 # Creating ldap structure according to EXAMPLE organization (example.com)
 cd $HOME_LDAP
 ldapadd -Y EXTERNAL -H ldapi:/// -D "cn=config" -f 00_hdb_monitor_log.ldif
 chown -Rv ldap.ldap /etc/openldap/slapd.d/cn\=config/cn\=schema/*
 chmod -Rv 600 /etc/openldap/slapd.d/cn\=config/cn\=schema/*
-cd /etc/openldap/slapd.d/cn\=config/cn\=schema/
+cd /etc/openldap/slapd.d/cn\=config/cn\=schema/                                                        
 chcon --reference=cn\=\{2\}inetorgperson.ldif cn\=\{*
 cd $HOME_LDAP
 echo "Restarting services" && sleep 1.2
 systemctl stop slapd  &&  systemctl start slapd && systemctl status slapd && sleep 1.2
-cd $HOME_LDIFSC
-# cn=manager,dc=example,dc=mx password user --> redhat
-# uid=replication,ou=Generic,dc=example,dc=mx password user --> R3pl1c4c10n.U3R
+#R3pl1c4c10n.U3R
 ldapadd -Y EXTERNAL -H ldapi:/// -D "cn=config" -f 01_sinlimite-busqueda.ldif
 ldapadd -Y EXTERNAL -H ldapi:/// -D "cn=config" -f 03a_modulo-replicacion.ldif
 ldapadd -Y EXTERNAL -H ldapi:/// -D "cn=config" -f 03b_overlay-replicacion.ldif
 ldapadd -Y EXTERNAL -H ldapi:/// -D "cn=config" -f 03c_modulo-replicacion.ldif
 ldapadd -Y EXTERNAL -H ldapi:/// -D "cn=config" -f 04_syncrep-maestro.ldif
 ldapadd -Y EXTERNAL -H ldapi:/// -D "cn=config" -f 05_total-acl.ldif
-cd $HOME_LDIFSD
 ldapadd -x -h $(hostname) -D "cn=manager,dc=example,dc=com" -f 02_rama_inicial.ldif -w redhat
-cd $HOME_LDIFSC
 ldapadd -x -h $(hostname) -D "cn=manager,dc=example,dc=com" -f 04d_usuario-rep.ldif -w redhat
-ldapadd -Y EXTERNAL -H ldapi:/// -D "cn=config" -f 06a_agregar_indices.ldif
+ldapadd -Y EXTERNAL -H ldapi:/// -D "cn=config" -f 07a_agregar_indices.ldif
 #ldapadd -Y EXTERNAL -H ldapi:/// -D "cn=config" -f 08a_activacion_tls_01.ldif
